16c8717c5b16a96479acbcc105324af9f100cfa5
khakisage <jongjin201@gmail.com>
2024-04-08 09:56:18 +0900

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let btn1 = document.querySelector(".move0404");
-btn.addEventListener("click", function () {
+btn1.addEventListener("click", function () {
   location.href = "til0404.html";
 });
 
 let btn2 = document.querySelector(".move0408");
-btn.addEventListener("click", function () {
+btn2.addEventListener("click", function () {
   location.href = "til0408.html";
 });
