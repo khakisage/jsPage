383dd7bee7e6ff5dc8d34ede610bdc74e2b4b683
khakisage <jongjin201@gmail.com>
2024-04-08 09:55:06 +0900

    update 2 files and create 1 file

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let btn = document.querySelector(".move0404");
+let btn1 = document.querySelector(".move0404");
 btn.addEventListener("click", function () {
   location.href = "til0404.html";
+});
+
+let btn2 = document.querySelector(".move0408");
+btn.addEventListener("click", function () {
+  location.href = "til0408.html";
 });
